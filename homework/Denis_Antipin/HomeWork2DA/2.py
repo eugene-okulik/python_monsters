9969c3842e4e7e68b15e6be6b73f26e0dc084bb3
Denis Antipin <denis_antipin@yahoo.com>
2023-05-29 15:47:31 +0400

    fixed file5

--- homework/Denis_Antipin/HomeWork2DA/2.py
+++ homework/Denis_Antipin/HomeWork2DA/2.py
@@ -1,3 +1,4 @@
 x = 7
 y = 3
-print(x-y / 1 + x*y)
+print(x - y / 1 + x * y)
+
--- homework/Denis_Antipin/HomeWork2DA/5.py
+++ homework/Denis_Antipin/HomeWork2DA/5.py
@@ -3,4 +3,4 @@ a = float(input('a: '))
 b = float(input('b: '))
 af = (a+b)/2
 gm = math.sqrt(a*b)
-print('Арифметическое= ', af, 'Геометрическое= ', gm )
+print('Арифметическое= ', af, 'Геометрическое= ', gm)
