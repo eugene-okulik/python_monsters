556087e790501f5a573155dc6b2436ccfd195323
Denis Antipin <denis_antipin@yahoo.com>
2023-06-04 15:49:13 +0400

    fixedf2t2

--- homework/Denis_Antipin/HomeWork2DA/5.py
+++ homework/Denis_Antipin/HomeWork2DA/5.py
@@ -1,6 +1,6 @@
 import math
 a = float(input('a: '))
 b = float(input('b: '))
-af = (a+b)/2
-gm = math.sqrt(a*b)
+af = (a + b)/2
+gm = math.sqrt(a * b)
 print('Арифметическое= ', af, 'Геометрическое= ', gm)
