fd17d859f47e91f71a0485b3c19c0f2f8cc179f3
Denis Antipin <denis_antipin@yahoo.com>
2023-06-08 08:43:17 +0400

    HW6f2.py

---
+++ homework/Denis_Antipin/HomeWork6DA/HW6f2.py
@@ -0,0 +1,6 @@
+from random import randint
+random_number = randint(1, 100)
+print(random_number)
+my_list = [80, 69, 19, 42, 15, 92, 90, 33, 98, 4]
+print(max(my_list), (min(my_list)), (sum(my_list)), sum(my_list)/len(my_list))
+
