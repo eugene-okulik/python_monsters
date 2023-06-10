3adb80e5dd1d983d1e758c1b02d083d99ddedca5
Dasha Adartsevich <dashaadartsevich@gmail.com>
2023-06-10 16:19:20 +0300

    dasha_homework_8_done

---
+++ homework/Dasha_Adartsevich/homework_6/test.py
@@ -0,0 +1,12 @@
+number_one = int(input('Enter first number '))
+number_two = int(input('Enter second number '))
+result = number_one * number_two
+
+
+def multiply(num):
+    return result * num
+
+
+print(multiply(2))
+
+
