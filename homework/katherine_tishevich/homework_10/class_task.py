e17d50d189c1bf923c5d71a530725b879e849e93
Kate Tishevich <kate.t@noboring-finance.ru>
2023-06-19 13:05:59 +0300

    update homework_10 for new specification

--- homework/katherine_tishevich/homework_10/class_task.py
+++ homework/katherine_tishevich/homework_10/class_task.py
@@ -13,6 +13,8 @@
 # После создания пометьте одну книгу как зарезервированную.
 # Распечатайте детали о каждой книге в таком виде:
 # Если книга зарезервирована:
+# Название: Идиот, Автор: Достоевский, страниц: 500, материал: бумага, зарезервирована
+# если не зарезервирована:
 # Название: Идиот, Автор: Достоевский, страниц: 500,  материал: бумага
 # Второй класс
 # Создайте дочерний класс для первого. Это будет класс для школьных учебников. В нем будут дополнительные атрибуты:
@@ -22,6 +24,8 @@
 # Создайте несколько экземпляров учебников.
 # После создания пометьте один учебник как зарезервированный.
 # Распечатайте детали о каждом учебнике в таком виде: Если учебник зарезервирован:
+# Название: Алгебра, Автор: Иванов, страниц: 200, предмет: Математика, класс: 9, зарезервирована
+# если не зарезервирован:
 # Название: Алгебра, Автор: Иванов, страниц: 200, предмет: Математика, класс: 9
 class Book:
     page_material = 'бумага'
@@ -34,6 +38,16 @@ class Book:
         self.ISBN = isbn
         self.reserve = reserve
 
+    def check_reserve_book(self):
+        return self.reserve
+
+    def __str__(self):
+        if self.check_reserve_book():
+            return f'Название: {self.book_name}, Автор: {self.author}, страниц: {self.page_count}, материал: ' \
+                   f'{self.page_material}, зарезервирована'
+        return f'Название: {self.book_name}, Автор: {self.author}, страниц: {self.page_count}, материал: ' \
+               f'{self.page_material}'
+
 
 first_book = Book(book_name='python_1', author='author_for_python_1', page_count=100, isbn='00000001',
                   reserve=False)
@@ -45,31 +59,22 @@ fourth_book = Book(book_name='python_4', author='author_for_python_4', page_coun
                    reserve=False)
 fifth_book = Book(book_name='python_5', author='author_for_python_5', page_count=500, isbn='00000005',
                   reserve=True)
-list_data = [first_book.__dict__,
-             second_book.__dict__,
-             third_book.__dict__,
-             fourth_book.__dict__,
-             fifth_book.__dict__]
-key = "reserve"
-value = True
-dict_with_true_conditions = next((d for d in list_data if d.get(key) == value), None)
-del dict_with_true_conditions["ISBN"]
-del dict_with_true_conditions["reserve"]
-dict_with_true_conditions.update({"page_material": Book.__dict__.get("page_material")})
-ideal_keys = ["Название", "Автор", "страниц", "материал"]
-dict_values = dict_with_true_conditions.values()
-new_dict = {ideal_keys: dict_values for ideal_keys, dict_values in zip(ideal_keys, dict_values)}
-update_format = []
-for key, item in new_dict.items():
-    update_format.append("{}: {}".format(key, item))
-result_books = ", ".join(update_format)
-print(result_books)
+books = [first_book, second_book, third_book, fourth_book, fifth_book]
+for book in books:
+    print(book)
 
 
 class SchoolBooks(Book):
     subject = 'Information Technology'
     level = 9
     tasks_in_book = True
+
+    def __str__(self):
+        if self.check_reserve_book():
+            return f'Название: {self.book_name}, Автор: {self.author}, страниц: {self.page_count}, предмет: ' \
+               f'{self.subject}, класс: {self.level}, зарезервирована'
+        return f'Название: {self.book_name}, Автор: {self.author}, страниц: {self.page_count}, предмет: ' \
+            f'{self.subject}, класс: {self.level}'
 
 
 first_lesson_book = SchoolBooks(book_name="Music", author="Rohmaninov", page_count=100, isbn='12323239',
@@ -78,23 +83,6 @@ second_lesson_book = SchoolBooks(book_name="History", author="Napoleon", page_co
                                  reserve=False)
 third_lesson_book = SchoolBooks(book_name="OOP", author="Оле Джохан Дал и Кристен Нюгорт", page_count=100000,
                                 isbn='100000000', reserve=True)
-
-
-list_data = [first_lesson_book.__dict__,
-             second_lesson_book.__dict__,
-             third_lesson_book.__dict__]
-key = "reserve"
-value = True
-dict_with_true_conditions_school_books = next((d for d in list_data if d.get(key) == value), None)
-del dict_with_true_conditions_school_books["ISBN"]
-del dict_with_true_conditions_school_books["reserve"]
-dict_with_true_conditions_school_books.update({"subject": SchoolBooks.__dict__.get("subject")})
-dict_with_true_conditions_school_books.update({"level": SchoolBooks.__dict__.get("level")})
-ideal_keys = ["Название", "Автор", "страниц", "предмет", "класс"]
-dict_values = dict_with_true_conditions_school_books.values()
-new_dict = {ideal_keys: dict_values for ideal_keys, dict_values in zip(ideal_keys, dict_values)}
-update_format = []
-for key, item in new_dict.items():
-    update_format.append("{}: {}".format(key, item))
-result_school_books = ", ".join(update_format)
-print(result_school_books)
+school_books = [first_lesson_book, second_lesson_book, third_lesson_book]
+for book in school_books:
+    print(book)
